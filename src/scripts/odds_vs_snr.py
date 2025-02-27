a0ed136d8d67ead61c38bf9ccf412bb462eedbfa
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-02-27 15:23:47 +1300

    add template scripts

---
+++ src/scripts/odds_vs_snr.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import paths
+
+
+def main():
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    plt.plot(x, y)
+    fig.savefig(paths.figures / "posterior_predictive.pdf", bbox_inches="tight", dpi=300)
+
+
+if __name__ == '__main__':
+    main()
