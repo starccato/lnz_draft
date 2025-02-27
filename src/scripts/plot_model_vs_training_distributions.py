2f03eeb1a96e50158ee90662ec783b860f22c9c9
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-02-27 23:41:35 +1300

    rename scripts

--- src/scripts/plot_model_vs_training_distributions.py
+++ src/scripts/plot_model_vs_training_distributions.py
@@ -7,7 +7,7 @@ def main():
     x = np.linspace(0, 10, 100)
     y = np.sin(x)
     plt.plot(x, y)
-    plt.savefig(paths.figures / "posterior_predictive.pdf", bbox_inches="tight", dpi=300)
+    plt.savefig(paths.figures / "model_vs_training_distributions.pdf", bbox_inches="tight", dpi=300)
 
 
 if __name__ == '__main__':
--- src/scripts/plot_odds_vs_snr.py
+++ src/scripts/plot_odds_vs_snr.py
@@ -7,7 +7,7 @@ def main():
     x = np.linspace(0, 10, 100)
     y = np.sin(x)
     plt.plot(x, y)
-    plt.savefig(paths.figures / "posterior_predictive.pdf", bbox_inches="tight", dpi=300)
+    plt.savefig(paths.figures / "odds_vs_snr.pdf", bbox_inches="tight", dpi=300)
 
 
 if __name__ == '__main__':
